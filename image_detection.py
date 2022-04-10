2c62e37da932f3e6b05b8602b4b1e4d78e0338da
qwertyinsomnia <190420691@qq.com>
2022-04-11 05:12:52 +0800

    2
    删除多余代码

--- image_detection.py
+++ image_detection.py
@@ -2,8 +2,9 @@ import onnxruntime as ort
 import cv2
 import numpy as np
 import argparse
+from sort import Sort
 
-class yolor():
+class yolonet():
     def __init__(self, model, confThreshold=0.5, nmsThreshold=0.5, objThreshold=0.5):
         self.classes = list(map(lambda x: x.strip(), open('label.txt', 'r').readlines()))
         self.inpWidth = 640
@@ -16,9 +17,15 @@ class yolor():
         self.objThreshold = objThreshold
         self.keep_ratio = True
         self.swaprgb = True
-    def _make_grid(self, nx=20, ny=20):
-        xv, yv = np.meshgrid(np.arange(ny), np.arange(nx))
-        return np.stack((xv, yv), 2).reshape((-1, 2)).astype(np.float32)
+
+        self.COLORS = np.random.randint(0, 255, size=(200, 3), dtype="uint8")
+        self.text_size = 1
+        self.tracker = Sort()
+
+    def change_text_size(self, size):
+        self.text_size = size
+
+
     def resize_image(self, srcimg):
         top, left, newh, neww = 0, 0, self.inpHeight, self.inpWidth
         if self.keep_ratio and srcimg.shape[0] != srcimg.shape[1]:
@@ -55,20 +62,49 @@ class yolor():
         boxes[:, 1] = (boxes[:, 1] - 0.5 * boxes[:, 3] - top) * ratioh
         boxes[:, 2] = boxes[:, 2] * ratiow
         boxes[:, 3] = boxes[:, 3] * ratioh
-        boxes = boxes.astype(np.int)
+        boxes = boxes.astype(np.int64)
         classIds = np.argmax(outs[:,5:], axis = 1)
         confidences = np.max(outs[:,5:], axis = 1)
 
 
-		# 使用NMS去除重复的标记框
+		# use NMS to remove overlap
         indices = cv2.dnn.NMSBoxes(boxes.tolist(), confidences.tolist(), self.confThreshold, self.nmsThreshold)
         results = []
         for i in indices:
             # i = i[0]
             box = boxes[i]
-            results.append((box[0], box[1], box[0] + box[2], box[1] + box[3], self.classes[classIds[i]]))
-            cv2.rectangle(frame, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (0, 0, 255), thickness=3)
-            cv2.putText(frame, self.classes[classIds[i]]+': '+str(round(confidences[i], 3)), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=3)
+            results.append((box[0], box[1], box[0] + box[2], box[1] + box[3], confidences[i], classIds[i]))
+            # cv2.rectangle(frame, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (0, 0, 255), thickness=2)
+            # cv2.putText(frame, self.classes[classIds[i]]+': '+str(round(confidences[i], 3)), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=2)
+        
+        # np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+        results = np.asarray(results)
+
+        # use sort to track
+        tracks = self.tracker.update(results)
+        boxes = []  # sorted box
+        indexIDs = []
+        cls_IDs = []  # class
+
+        for track in tracks:
+            # print(track)
+            boxes.append([track[0], track[1], track[2], track[3]])
+            indexIDs.append(int(track[4]))
+            cls_IDs.append(int(track[5]))
+
+        if len(boxes) > 0:
+            for i, box in enumerate(boxes):
+                (x, y) = (int(box[0]), int(box[1]))
+                (w, h) = (int(box[2]), int(box[3]))
+
+                # draw rectangle and text
+                color = [int(c) for c in self.COLORS[indexIDs[i] % len(self.COLORS)]]
+                cv2.rectangle(frame, (x, y), (w, h), color, 4)
+
+                text = "{}-{}".format(self.classes[cls_IDs[i]], indexIDs[i])
+                cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, self.text_size, color, thickness=2)
+                # cv2.putText(frame, self.classes[cls_IDs[i]]+': '+str(round(confidences[i], 3)), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness=2)
+
         return frame
 
 if __name__=='__main__':
@@ -79,8 +115,8 @@ if __name__=='__main__':
     parser.add_argument('--objThreshold', default=0.5, type=float, help='object confidence')
     args = parser.parse_args()
 
-    # net = yolor(confThreshold=args.confThreshold, nmsThreshold=args.nmsThreshold, objThreshold=args.objThreshold)
-    net = yolor('yolov5s.onnx', 0.5, 0.5, 0.5)
+    # net = yolonet(confThreshold=args.confThreshold, nmsThreshold=args.nmsThreshold, objThreshold=args.objThreshold)
+    net = yolonet('yolov5s.onnx', 0.5, 0.5, 0.5)
     srcimg = cv2.imread('WeChat Image_20220210121311.jpg')
     srcimg = net.detect(srcimg)
 
--- tkui.py
+++ tkui.py
@@ -5,12 +5,13 @@ from tkinter import StringVar
 import cv2
 from PIL import Image, ImageTk
 import time
-from image_detection import yolor
+from image_detection import yolonet
 
 class Surface(ttk.Frame):
     pic_path = ""
-    viewhigh = 800
-    viewwide = 800
+    vid_path = ""
+    viewhigh = 1000
+    viewwide = 1200
     screen_pos_x = 100
     screen_pos_y = 20
     update_time = 0
@@ -28,7 +29,7 @@ class Surface(ttk.Frame):
         win.geometry(screen_resolution)
 
         # Load model
-        self.net = yolor('yolov5s.onnx', 0.5, 0.5, 0.5)
+        self.net = yolonet('models/yolov5s.onnx', 0.5, 0.5, 0.5)
         
         self.pack(fill=tk.BOTH, expand=tk.YES, padx="5", pady="5")
         frame_left.pack(side="left",expand=1,fill="both")
@@ -38,20 +39,21 @@ class Surface(ttk.Frame):
         # ttk.Label(frame_right1, text='license plate position:').grid(column=0, row=0, sticky=tk.W)
         ttk.Label(frame_right2, text='choose model:').pack(anchor="se") 
         
-        # data = ["kNearest", "SVM"]
-        # self.value = StringVar()
-        # self.value.set("kNearest")
-        # self.model_ctl = ttk.Combobox(frame_right2, width=17, height=8, state='normal', textvariable=self.value)
-        # self.model_ctl.bind("<<ComboboxSelected>>", self.SetModel)
-        # self.model_ctl["values"] = data
-        # self.model_ctl.current(0)
-        # self.model_ctl.pack()
+        data = ["yolov5s", "yolov5l"]
+        self.value = StringVar()
+        self.value.set("yolov5s")
+        self.model_ctl = ttk.Combobox(frame_right2, width=17, height=8, state='normal', textvariable=self.value)
+        self.model_ctl.bind("<<ComboboxSelected>>", self.SetModel)
+        self.model_ctl["values"] = data
+        self.model_ctl.current(0)
+        self.model_ctl.pack()
 
         from_pic_ctl = ttk.Button(frame_right2, text="load image", width=20, command=self.from_pic)
+        from_vid_ctl = ttk.Button(frame_right2, text="load video", width=20, command=self.from_video)
         self.image_ctl = ttk.Label(frame_left)
         self.image_ctl.pack(anchor="nw")
 
-        start_recog_ctl = ttk.Button(frame_right2, text="start", width=20, command=self.start_recognition)
+        # start_recog_ctl = ttk.Button(frame_right2, text="start", width=20, command=self.start_recognition)
         
         # self.roi_ctl = ttk.Label(frame_right1)
         # self.roi_ctl.grid(column=0, row=1, sticky=tk.W)
@@ -62,28 +64,34 @@ class Surface(ttk.Frame):
         # self.color_ctl.grid(column=0, row=4, sticky=tk.W)
 
         from_pic_ctl.pack(anchor="se", pady="5")
-        start_recog_ctl.pack(anchor="se", pady="5")
+        from_vid_ctl.pack(anchor="se", pady="5")
+        # start_recog_ctl.pack(anchor="se", pady="5")
+
 
 
     def start_recognition(self):
         srcimg = cv2.imread(self.pic_path)
+        high, wide, _ = srcimg.shape
+        # print(srcimg.shape)
+        if wide > 2000 or high > 2000:
+            self.net.change_text_size(2)
+        else:
+            self.net.change_text_size(1)
         roi = self.net.detect(srcimg)
+        return roi
         
-        self.show_roi(roi)
 
-    # def SetModel(self, event):
-    #     model = self.value.get()
-    #     # print('选中的数据:{}'.format(self.model_ctl.get()))
-    #     # print('value的值:{}'.format(self.value.get()))
-    #     # print(model)
-    #     if model == "kNearest":
-    #         self.model = self.cnnModel
-    #         self.modelType = MODEL_CNN
-    #         print("select model: kNearest")
-    #     elif model == "SVM":
-    #         self.model = self.svmModel
-    #         self.modelType = MODEL_SVM
-    #         print("select model: SVM")
+    def SetModel(self, event):
+        model = self.value.get()
+        # print('选中的数据:{}'.format(self.model_ctl.get()))
+        # print('value的值:{}'.format(self.value.get()))
+        # print(model)
+        if model == "yolov5s":
+            self.net = yolonet('models/yolov5s.onnx', 0.5, 0.5, 0.5)
+            print("select model: yolov5s")
+        elif model == "yolov5l":
+            self.net = yolonet('models/yolov5l.onnx', 0.5, 0.5, 0.5)
+            print("select model: yolov5l")
 
     def get_imgtk(self, img_bgr):
         img = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
@@ -107,27 +115,75 @@ class Surface(ttk.Frame):
     def show_roi(self, roi):
         roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
         roi = Image.fromarray(roi)
-        # self.imgtk_roi = ImageTk.PhotoImage(image=roi)
-        roi = roi.resize((800, 800), Image.ANTIALIAS)
+        imgtk = ImageTk.PhotoImage(image=roi)
+        wide = imgtk.width()
+        high = imgtk.height()
+        if wide > self.viewwide or high > self.viewhigh:
+            wide_factor = self.viewwide / wide
+            high_factor = self.viewhigh / high
+            factor = min(wide_factor, high_factor)
+            
+            wide = int(wide * factor)
+            if wide <= 0 : wide = 1
+            high = int(high * factor)
+            if high <= 0 : high = 1
+            roi = roi.resize((wide, high), Image.ANTIALIAS)
         self.imgtk_roi = ImageTk.PhotoImage(image=roi)
-        # self.roi_ctl.configure(image=self.imgtk_roi, state='enable')
         self.image_ctl.configure(image=self.imgtk_roi, state='enable')
-        # self.r_ctl.configure(text=str(r))
         self.update_time = time.time()
         
     def from_pic(self):
         self.thread_run = False
-        self.pic_path = askopenfilename(title="choose the image", filetypes=[('All files','*.*'), ("jpg", "*.jpg"), ('png','*.png')])
-        if self.pic_path:
-            # r, roi = PlateRecognition.PlateRecognition(self.pic_path, False, self.model, self.modelType)
-            img_bgr = cv2.imread(self.pic_path)
-            self.imgtk = self.get_imgtk(img_bgr)
-            self.image_ctl.configure(image=self.imgtk)
+        self.pic_path = askopenfilename(title="choose the image", filetypes=[("jpg", "*.jpg"), ('png','*.png')]) # ('All files','*.*'), 
+        # if self.pic_path:
+        #     # r, roi = PlateRecognition.PlateRecognition(self.pic_path, False, self.model, self.modelType)
+        #     img_bgr = cv2.imread(self.pic_path)
+        #     self.imgtk = self.get_imgtk(img_bgr)
+        #     self.image_ctl.configure(image=self.imgtk)
             # self.show_roi(r, roi)
+        roi = self.start_recognition()
+        self.show_roi(roi)
+        # print("done")
+
+    def from_video(self):
+        self.thread_run = False
+        self.vid_path = askopenfilename(title="choose the image", filetypes=[('mp4','*.mp4')])
+        if self.vid_path:
+            self.video = cv2.VideoCapture(self.vid_path)
+            ok, frame = self.video.read()
+            if ok:
+                # print(frame.shape)
+                high, wide, _ = frame.shape
+                if wide > 2000 or high > 2000:
+                    self.net.change_text_size(2)
+                else:
+                    self.net.change_text_size(1)
+                if wide > self.viewwide or high > self.viewhigh:
+                    wide_factor = self.viewwide / wide
+                    high_factor = self.viewhigh / high
+                    factor = min(wide_factor, high_factor)
+                    
+                    wide = int(wide * factor)
+                    if wide <= 0 : wide = 1
+                    high = int(high * factor)
+                    if high <= 0 : high = 1
+            self.video_loop(wide, high)
+
+    def video_loop(self, wide, high):
+        ok, frame = self.video.read()
+        if ok: # frame captured without any errors
+            roi = self.net.detect(frame)
+            cv2image = cv2.cvtColor(roi, cv2.COLOR_BGR2RGBA)
+            cv2image = cv2.resize(cv2image, (wide, high), interpolation=cv2.INTER_LANCZOS4)
+            self.current_image = Image.fromarray(cv2image)
+            self.video_roi = ImageTk.PhotoImage(image=self.current_image)
+            self.image_ctl.config(image=self.video_roi)
+            self.after(1, self.video_loop, wide, high)
+
 
 def close_window():
     print("destroy")
-    if surface.thread_run :
+    if surface.thread_run:
         surface.thread_run = False
         surface.thread.join(2.0)
     win.destroy()
